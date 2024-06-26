a0ffdaa3753cc92e0948e4b4a0a5e9bbc074a0f0
Alisa.Schmidt <aschmidt@crg.es>
2024-06-26 14:40:45 +0200

    include stats

--- R_code/Fig_4/Fig4K_NAD_sensor.R
+++ R_code/Fig_4/Fig4K_NAD_sensor.R
@@ -7,7 +7,8 @@ library(gridExtra)
 
 # NAD Ratio ---------------------------------------------------------------
 # 2h raw file
-data_recon_2h_raw <- read.delim("Data/Objects_Population - Nuclei Selected_reconssensor_2h.txt", skip = 9)
+data_recon_2h_raw <- read.delim("/Users/alisc/Downloads/IMPDH2/IMPDH2/Data/Objects_Population - Nuclei Selected_reconssensor_2h.txt", skip = 9)
+
 
 # 2h processed file
 data_recon_2h <- data_recon_2h_raw %>%  
@@ -21,12 +22,12 @@ data_recon_2h <- data_recon_2h_raw %>%
                                Column %in% c(7,8,9) ~ "Eto_10uM"),
          Cell = factor(Cell, levels = c("WT_empty","KO_empty", "KO_WT","KO_NLS", "WT_non-transfected")),
          Treatment = factor(Treatment, levels = c("DMSO","Eto_10uM"))) %>% 
-  rename("NuclearArea" = "Nuclei.Selected...nuclear_areainum2n.Area..?m..",
-         "Intensity488" = "Nuclei.Selected...Intensity.Nucleus.Alexa.488.Mean",
-         "Intensity555" = "Nuclei.Selected...Intensity.Nucleus.Alexa.555.Mean",
-         "YFP_mCherry" = "Nuclei.Selected...yfp.mcherry") %>% 
+ # rename("NuclearArea" = "Nuclei.Selected...nuclear_areainum2n.Area..µm..",
+  #       "Intensity488" = "Nuclei.Selected...Intensity.Nucleus.Alexa.488.Mean",
+   #      "Intensity555" = "Nuclei.Selected...Intensity.Nucleus.Alexa.555.Mean",
+  #       "YFP_mCherry" = "Nuclei.Selected...yfp.mcherry") %>% 
 
-  filter(NuclearArea < 250) %>%
+  filter(Nuclei.Selected...nuclear_areainum2n.Area..µm.. < 250) %>%
   # count number of samples
   group_by(Cell, Treatment) %>%
   add_tally() 
@@ -35,27 +36,33 @@ data_recon_2h <- data_recon_2h_raw %>%
 data_recon_2h_remove_outlier <- data_recon_2h %>%
   
   group_by(Cell, Treatment) %>%
-  mutate(median_yfp_mcherry = median(YFP_mCherry),
-         sd_yfp_mcherry = sd(YFP_mCherry)) %>%
+  mutate(median_yfp_mcherry = median(Nuclei.Selected...yfp.mcherry),
+         sd_yfp_mcherry = sd(Nuclei.Selected...yfp.mcherry)) %>%
   
-  filter(YFP_mCherry >= median_yfp_mcherry - (3 * sd_yfp_mcherry) &
-           YFP_mCherry <= median_yfp_mcherry + (3 * sd_yfp_mcherry)) %>%
+  filter(Nuclei.Selected...yfp.mcherry >= median_yfp_mcherry - (3 * sd_yfp_mcherry) &
+           Nuclei.Selected...yfp.mcherry <= median_yfp_mcherry + (3 * sd_yfp_mcherry)) %>%
   ungroup() 
 
 
 # Print plots to PDF
+pdf("/Users/alisc/Desktop/CRG/Operetta/NAD_sensor/NAD_sensor_stats.pdf")
 ggplot(data_recon_2h_remove_outlier %>% 
          filter(Cell != "WT_non-transfected"), 
-       aes(Cell, YFP_mCherry, fill = Treatment)) + 
-  geom_point(aes(colour = Treatment),
-             alpha=0.1, 
-             position = position_jitterdodge(dodge.width=0.8)) +
+       aes(Cell, Nuclei.Selected...yfp.mcherry, fill = Treatment)) + 
+ # geom_point(aes(colour = Treatment),
+  #           alpha=0.1, 
+   #          position = position_jitterdodge(dodge.width=0.8)) +
   geom_boxplot(outlier.shape = NA) +
   scale_fill_brewer(palette="Accent") +
   scale_colour_brewer(palette="Accent") +
+  stat_compare_means(method = "wilcox", aes(group = Treatment), label = "p.signif") +
   labs(title="2h; 3SD removed;\nShowing all data") +
   theme_classic()+
-  ylab("NAD/NAD+ ratio")+
+  ylab("ATP/NAD ratio")+
   xlab("")
-
+dev.off()
+data_recon_2h_remove_outlier$Identifier <- paste(data_recon_2h_remove_outlier$Cell, data_recon_2h_remove_outlier$Treatment)
+stats <- compare_means(data = data_recon_2h_remove_outlier, Nuclei.Selected...yfp.mcherry ~ Identifier, method = "wilcox.test")
+#%>% 
+ # slice(28,32)
 
